1bee6a50524685a9d6be34f6bcbff1eccffdadfb
Carlie-Dev <ellepheonixgames@gmail.com>
2024-03-04 12:16:33 -0600

    completed Timer Swap

--- imageapp.js
+++ imageapp.js
@@ -11,7 +11,7 @@ let skinSet = ['ellephoenix.png','mobster.png','undead.png','woman.png']
 let buildSet = ['diningroom.png','hallway.png', 'beefarm.png','willowisps.png']
 
 //Multidimensional array
-let twoArrays = [skinSet,buildSet]
+let imageSetsArray = [skinSet,buildSet]
 
 
 /**
@@ -27,6 +27,7 @@ function UpdateThumbnails(ev){
     skinSetFileName = skinSet[0]
     console.log('Switcher source: ' + switcher);
     console.log('SkinSet Source: ' + skinSet[0]);
+    console.log(ev.target +' was clicked')
 
     //multidimentional array stores the objects that will be changed
     let imageThumbnails = [document.getElementById('ellephoenix'),document.getElementById('mobster'),document.getElementById('ice_king'),document.getElementById('woman')];
@@ -41,7 +42,7 @@ function UpdateThumbnails(ev){
 
 
     for(i = 0; i < imageThumbnails.length; i++){
-        imageThumbnails[i].src = 'images/' + twoArrays[j][i];
+        imageThumbnails[i].src = 'images/' + imageSetsArray[j][i];
     }
 
 
@@ -113,6 +114,9 @@ document.getElementById('woman').addEventListener('click', (ev)=>{
     
 });
 
+
+
+
 //update
 loadSettings()
 window.addEventListener('load',UpdateThumbnails)
@@ -133,6 +137,71 @@ function loadSettings(){
 
     myImage.className = 'image1';
 
-    mainDiv.appendChild(myImage)
-
+    mainDiv.appendChild(myImage);
 }
+
+
+//Timer stuff
+
+//prints out all values in the imageSetArray for reference
+for(let i = 0; i < imageSetsArray.length; i ++){
+    for(let j = 0; j <imageSetsArray[i].length; j++){
+        console.log("From multidimensional array: " + imageSetsArray[i][j]);
+    }
+}
+
+
+/**
+* Purpose- Extract a random string from an array. 
+* @param {Array[array]} expects an array of array of strings that will be image src file names
+* @returns {string} returns a random string from the array
+* @example
+* 
+*/
+function randomImage(array){
+    //Choses random number based on the length of the array
+    let i = Math.floor(Math.random() * array.length);
+    //Get an array based on the random number
+    let j = Math.floor(Math.random() * array[i].length);
+    //returns the string.
+    return array[i][j]
+}
+
+/**
+ * 
+ * @param {} - void
+ * @returns {} -void
+ * constantly running interval function that updates the cycle of images
+ * Timer is started when page is read starts
+*/
+
+setInterval(()=>{
+    let mainDiv = document.getElementById('mainImageDisplay');
+    mainDiv.innerHTML = "";
+    
+
+    let myImage = document.createElement("img");
+    myImage.src = "images/" + randomImage(imageSetsArray);
+    checker = "images/" + myImage.src.split('/').pop();
+    console.log('images/' + imageSetsArray[0][1]);
+    console.log(myImage.src);
+    console.log(checker);
+
+    myImage.alt = 'switching through Array';
+
+    if (checker == 'images/' + imageSetsArray[0][0]){
+        console.log(imageSetsArray[0][0])
+        myImage.className = 'image1';
+    }else if(checker == 'images/' + imageSetsArray[0][1]){
+        myImage.className = 'image2';
+    }else if(checker == 'images/' + imageSetsArray[0][2]){
+        myImage.className = 'image3';
+    }else if(checker == 'images/' + imageSetsArray[0][3]){
+        myImage.className = 'image4';
+    }else{
+        myImage.className = 'default';
+    }
+    
+
+    mainDiv.appendChild(myImage);
+}, 3000)
